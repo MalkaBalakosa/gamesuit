ef523981786bf763e5514d382d9691a098617fab
MalkaBalakosa <92303560+MalkaBalakosa@users.noreply.github.com>
2021-10-27 11:36:53 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+var player = prompt('pilih :  gunting, batu, kertas');
+var computer = Math.random();
+if(computer < 0.34) {
+    computer ='gunting';
+} else if (computer >= 0.34 && computer < 0.67) {
+    computer ='batu';
+} else {
+    computer='kertas';
+}
+var hasil = '';
+if( player == computer ) {
+    hasil='menjawab sama dengan computer dan hasilnya seri';
+} else if (player === 'gunting') {
+    hasil=(computer == 'kertas') ? 'sangat hebat dan jawaban anda benar' : 'Anda nub sekali';
+} else if (player === 'batu') {
+    hasil=(computer == 'gunting') ? 'sangat hebat dan jawaban anda benar' : 'Anda nub sekali';
+} else if (player === 'kertas') {
+    hasil=(computer == 'batu') ? 'sangat hebat dan jawaban anda benar' : 'Anda nub sekali'; 
+} else {
+    hasil='anda salah masukin hahahaha';
+}
+
+alert('Kamu memilih ' + player + ' dan komputer memilih ' + computer  + ' \nkamu ' + hasil );
